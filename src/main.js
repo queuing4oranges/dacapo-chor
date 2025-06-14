3d9d31f728f89d02bcf547bf3cb94a33b9a3fd80
queuing4oranges <katjazenker@gmail.com>
2025-06-14 19:11:33 +0200

    Set up bootstrap for vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue'
-import './assets/main.scss'
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.vue'
+import './assets/main.scss'
 import router from './router/router';
+// Bootstrap icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+// Bootstrap for Vue
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import * as BootstrapVueNext from 'bootstrap-vue-next';
 
-createApp(App).use(router).mount('#app');
+//createApp(App).use(router).mount('#app');
+const app = createApp(App)
+
+app.use(router)
+app.use(BootstrapVueNext.BootstrapVueNext)
+
+app.mount('#app')
