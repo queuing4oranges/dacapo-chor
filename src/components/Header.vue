79586ae19eb817eb41083348d62a487f702d4462
queuing4oranges <katjazenker@gmail.com>
2025-06-27 22:37:16 +0200

    Add nav links to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,55 @@
 <script setup>
+    import { computed, watch } from 'vue';
+    import { useRoute } from 'vue-router';
     import MusicalNoteButton from './reusables/MusicalNoteButton.vue';
+
+    const route = useRoute();
+    const isNotRoute = computed(() => route.path === '/dacapo')
 </script>
 
 <template>
-    <header class="header d-flex justify-content-between p-2 mx-auto">
+    <header class="header d-flex justify-content-between p-2 mx-auto mb-4">
         <router-link to="/dacapo" class="text-decoration-none">
             <span class="d-flex flex-column p-1 note-btn" title="Home">
                 <MusicalNoteButton height="40" width="40" />
-                Home
             </span>
         </router-link>
-            <a href="https://www.instagram.com/dacapo.chor/" target="_blank" rel="noopener noreferrer">
-                <i class="fs-1 bi bi-instagram" title="Mehr von uns auf Instagram"></i>
-            </a>
+        <template v-if="!isNotRoute">
+            <span class="link-container">
+                <router-link to="/über-uns">Über uns</router-link>
+                <router-link to="/repertoire">Repertoire & Auftritte</router-link>
+                <router-link to="/kontakt">Kontakt</router-link>
+                <router-link to="/mitglieder-gesucht">Mitglieder gesucht</router-link>
+            </span>
+        </template>
+        <a href="https://www.instagram.com/dacapo.chor/" target="_blank" rel="noopener noreferrer">
+            <i class="fs-1 bi bi-instagram" title="Mehr von uns auf Instagram"></i>
+        </a>
     </header>
 </template>
 
 <style lang="scss" scoped>
     .header { 
         width: 90%;
+        display: flex;
+        align-items: center;
 
         span.note-btn {
             &:hover {
                 opacity: 0.8;
+            }
+        }
+
+        .link-container {
+            width: 30%;
+            display: flex;
+            justify-content: space-between;
+            a {
+                text-decoration: none;
+                color: $logo-color;
+                &:hover {
+                    opacity: 0.8;
+                }
             }
         }
 
--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '@/components/Hero.vue';
-import DaCapo from '@/components/DaCapo.vue';
+import HeroSquares from '@/components/HeroSquares.vue';
 import Kontakt from '@/components/Kontakt.vue';
 import About from '@/components/About.vue';
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
-    { path: '/dacapo', name: 'DaCapo', component: DaCapo },
+    { path: '/dacapo', name: 'DaCapo', component: HeroSquares },
     { path: '/über-uns', name: 'About', component: About },
     { path: '/kontakt', name: 'Kontakt', component: Kontakt }
     //{ path: '/über-uns', name: 'About', component: About },
